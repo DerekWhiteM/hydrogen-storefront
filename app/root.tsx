432517e7a1c9a995a85e6fce6b9a829938401fdc
Derek White <derekwhite.m@gmail.com>
2023-03-22 18:55:15 -0400

    create product template

--- app/root.tsx
+++ app/root.tsx
@@ -16,6 +16,15 @@ import tailwind from './styles/tailwind-build.css';
 import favicon from '../public/favicon.svg';
 import {Layout} from './components/Layout';
 import {Seo} from '@shopify/hydrogen';
+import {ShopifyProvider} from '@shopify/hydrogen-react';
+
+const shopifyConfig: any = {
+  storefrontToken: '3b580e70970c4528da70c98e097c2fa0',
+  storeDomain: 'https://hydrogen-preview.myshopify.com',
+  storefrontApiVersion: '2023-01',
+  countryIsoCode: 'US',
+  languageIsoCode: 'en',
+};
 
 export const links: LinksFunction = () => {
   return [
@@ -43,27 +52,29 @@ export async function loader({context}: LoaderArgs) {
 }
 
 export default function App() {
-  const data = useLoaderData<typeof loader>();
-
-  const {name} = data.layout.shop;
+  const data = useLoaderData();
+  const {name, description} = data.layout.shop;
 
   return (
-    <html lang="en">
-      <head>
-        <Seo />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Layout title={name}>
-          <Outlet />
-        </Layout>
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
+    <ShopifyProvider {...shopifyConfig}>
+      <html lang="en">
+        <head>
+          <Seo />
+          <Meta />
+          <Links />
+        </head>
+        <body>
+          <Layout title={name}>
+            <Outlet />
+          </Layout>
+          <ScrollRestoration />
+          <Scripts />
+        </body>
+      </html>
+    </ShopifyProvider>
   );
 }
+
 
 const LAYOUT_QUERY = `#graphql
   query layout {
